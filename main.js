6878b3a8ca664dcbcdce4f3e4e2e85cd88a2bec8
coderdannie <111102013+coderdannie@users.noreply.github.com>
2022-08-17 01:32:17 -0700

    js change

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict';
+const toggleBar = document.querySelector('.header__bar');
+const ulContainer = document.querySelector('.header__mobile-nav');
+toggleBar.addEventListener('click', () => {
+  if (!toggleBar.classList.contains('active')) {
+    toggleBar.classList.add('active');
+    ulContainer.style.visibility = 'visible';
+    ulContainer.style.opacity = '1';
+  } else {
+    toggleBar.classList.remove('active');
+    ulContainer.style.visibility = 'hidden';
+    ulContainer.style.opacity = '0';
+  }
+});
